378041ed55e82984d1fb95634a61cfdd36370cea
Pikachuxxxx <phani.s2909@icloud.com>
2025-01-25 22:54:15 +0530

    Morph target collision test

---
+++ Game/Assets/Scripts/VehicleMorph.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class VehicleMorph : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject);
+        if(other.gameObject.CompareTag("RockAI")) {
+            SkinnedMeshRenderer skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+            Debug.Log("SJFDBJHFGKEBFJKLEHSLFHELJ");
+            StartCoroutine(AnimateBlendShape(skinnedMeshRenderer));
+        }
+    }
+
+    private System.Collections.IEnumerator AnimateBlendShape(SkinnedMeshRenderer skinnedMeshRenderer)
+    {
+        float blendValue = 0f;
+        float currBlendValue = skinnedMeshRenderer.GetBlendShapeWeight(0);
+        while (blendValue < 100f && currBlendValue < 100f)
+        {
+            blendValue += Time.deltaTime * 250f;
+            skinnedMeshRenderer.SetBlendShapeWeight(0, blendValue);
+            yield return null;
+        }
+    }
+}
